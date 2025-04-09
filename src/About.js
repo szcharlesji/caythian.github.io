f10677f8ce583e126361007876a426348ff04786
Caythian <xcatherine020202@gmail.com>
2025-04-08 20:33:17 -0400

    update about and exhibitions

--- src/About.js
+++ src/About.js
@@ -17,7 +17,9 @@ function About(){
             <div className="des-wrapper">
                 <div className="header">Artist Statement:</div>
                 <div className="generaldes">Xuecong Wang <span className="generaldessc">王雪聪</span> (IPA:/wɑŋ çyɛ tsʰʊŋ/) lives and works in Sichuan, China, and New York, US. </div>
-                <div className="generaldes">In an era marked by the rapid advancement of technology in domains such as the internet, social media, and artificial intelligence, the nature of things and people becomes ambiguous. Our knowledge and beliefs are heavily influenced by our surroundings—the tangible world and the cultural and historical contexts in which we find ourselves. There is no absolute truth, only the truth we believe in. This realization can make one feel small and seemingly irrelevant, but it also provides an advantage, allowing for a momentary retreat from the turmoil to observe the world from a distance. This is what I do in art—to find the “observed truth”.</div>
+                <div className="generaldes">
+                    In an era marked by the rapid advancement of technology in domains such as the internet, social media, and artificial intelligence, the nature of things and people becomes ambiguous. Our knowledge and beliefs are heavily influenced by our surroundings—the tangible world and the cultural and historical contexts in which we find ourselves. There is no absolute truth, only the truth we believe in. This realization can make one feel small and seemingly irrelevant, but it also provides an advantage, allowing for a momentary retreat from the turmoil to observe the world from a distance. This is what I do in art—to find the “observed truth”.
+                </div>
                 <div className="generaldes">In my work, I reimagine the physical, cultural, and social environments that surround me. My pieces feature realistic portrayals of subjects and settings, yet they are situated within a surreal, dream-like pseudo-reality. The ambiguity of these spaces is achieved through the manipulation of light, which can be either insufficient or excessive.</div>
                 <div className="generaldes">Occasionally, I employ a first-person perspective to reveal my cognitive and emotional responses to an environment. Through this approach, I explore the multifaceted nature of my own identity and dissect my psyche.  I hope that the audience will gain insights about the multifaceted nature of the world from these perspectives as well.</div>
             </div>
@@ -32,10 +34,6 @@ function About(){
                 <a className="icon-wrapper"href="mailto:xcatherine020202@gmail.com">
                     <img className="icon" src={email} alt="email icon"></img>
                     {/* <div className="generaldes">Email</div> */}
-                </a>
-                <a className="icon-wrapper"href="https://www.youtube.com/channel/UCxSwejYGGTz7fBi_y0iI7uQ" target="_blank" rel="noopener noreferrer">
-                    <img className="icon" src={youtube} alt="youtube icon"></img>
-                    {/* <div className="generaldes">YouTube</div> */}
                 </a>
             </div>
         </div>
--- src/Cv.js
+++ src/Cv.js
@@ -13,6 +13,8 @@ function Cv(){
             <div className="generaldes">2021-2025 New York University</div>
             <div className="generaldes extra-indent">BFA: Studio Art, Minor: Psychology</div>
             <div className="header">Exhibitions:</div>
+            <div className="generaldes">2025</div>
+                <div className="generaldes extra-indent">Meditations in Blue, Saint Marks Arts, New York, NY</div>
             <div className="generaldes">2024</div>
                 <div className="generaldes extra-indent">Multiplicities, Commons Gallery, New York, NY</div>
                 <div className="generaldes extra-indent">Realism versus the Abstract, BWAC, Brooklyn, NY</div>
@@ -20,7 +22,7 @@ function Cv(){
             <div className="generaldes">2023</div>
                 <div className="generaldes extra-indent">Fingerfertigkeiten, St. Agnes, Berlin, Germany</div>
             <div className="generaldes">2022</div>
-                <div className="generaldes extra-indent">NYU Earth Day Fair,Schwarz Plaza, New York, NY</div>
+                <div className="generaldes extra-indent">NYU Earth Day Fair, Schwarz Plaza, New York, NY</div>
         </div>
         <div className="divider"></div>
         <div className="des-wrapper">
@@ -28,7 +30,6 @@ function Cv(){
             <div className="generaldes">2024</div>
             <div className="generaldes extra-indent">Chen Cui Artist Practice Award</div>
         </div>
-        <div className="generaldes">This page is in progress...</div>
         <div className="divider"></div>
 
         <Footer/>
