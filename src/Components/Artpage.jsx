eb70a4967f8ad8006c6bda9b92f5ab365c098dda
Caythian <xcatherine020202@gmail.com>
2025-06-03 23:13:20 -0400

    add blog

---
+++ src/Components/Artpage.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./Starter.css";
+
+function Starter (){
+    return(
+        <div className="Starter">
+            <div className="Startertitle">Xuecong Wang</div>
+            <div className="Startertitlesc">王雪聪</div>
+        </div>
+    );
+}
+export default Starter;
