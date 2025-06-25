dfa7292fb41bde3def0fc3206f2052575ce16a0b
Caythian <xcatherine020202@gmail.com>
2025-06-25 14:17:15 -0400

    menubar adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import "./App.css";
 import Headbar from "./Components/Headbar";
 import Footer from "./Components/Footer";
@@ -7,10 +7,29 @@ import Starter from "./Components/Artpage";
 import Filter from "./Components/Filter";
 
 function App() {
+  const [headbarOnTop, setHeadbarOnTop] = useState(false);
+
+  useEffect(() => {
+      const handleScroll = () => {
+        const starterHeight = document.querySelector('.Starter')?.offsetHeight || 0;
+        if (window.scrollY > 10) {
+          setHeadbarOnTop(true);
+        } else {
+          setHeadbarOnTop(false);
+        }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
+
   return (
     <div>
-      <Headbar />
       <Starter />
+      <Headbar style={{ zIndex: headbarOnTop ? 2 : 1 }} />
       <Filter />
       <Artworkdisplay />
       <Footer />
