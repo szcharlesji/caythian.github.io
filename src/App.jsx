cab68344b0000fa3e79a48c22bdf4112869d4080
Caythian <xcatherine020202@gmail.com>
2025-06-25 15:11:35 -0400

    menubar mobile improve

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,10 @@ import Starter from "./Components/Artpage";
 import Filter from "./Components/Filter";
 
 function App() {
-  const [headbarOnTop, setHeadbarOnTop] = useState(false);
-
-  useEffect(() => {
-      const handleScroll = () => {
-        const starterHeight = document.querySelector('.Starter')?.offsetHeight || 0;
-        if (window.scrollY > 10) {
-          setHeadbarOnTop(true);
-        } else {
-          setHeadbarOnTop(false);
-        }
-      };
-
-      window.addEventListener('scroll', handleScroll);
-
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-  }, []);
-
   return (
     <div>
       <Starter />
-      <Headbar style={{ zIndex: headbarOnTop ? 2 : 1 }} />
+      <Headbar />
       <Filter />
       <Artworkdisplay />
       <Footer />
